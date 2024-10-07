5f8356a1543d4473120f5db15f982c9bb2b5fb9d
GeorgiosDelkos <georgios.delkos@ourovoros.io>
2024-10-07 08:54:09 +0300

    Refactor CLI struct and command line options

--- src/cli.rs
+++ src/cli.rs
@@ -2,26 +2,26 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[clap(name = "Dyno Site Generator")]
+#[clap(name = "Dynosite Generator")]
 #[clap(
     author = "Georgios Delkos <georgios@tenbeo.io>, Camden Smallwood <camden-smallwood@gmail.com>"
 )]
 #[clap(version = "1.0")]
-#[clap(about = "Fuel Dyno Profiler Site Generator", long_about = None)]
+#[clap(about = "Fuel Dynosite Profiler Site Generator", long_about = None)]
 pub struct Options {
     #[clap(short, long)]
-    /// The folder containing the benchmarks
+    /// The target folder containing the benchmarks
     pub benchmarks_folder: PathBuf,
 
     #[clap(short, long)]
     /// The PR hash
     pub pr_hash: String,
 
-    /// The pr title
+    /// The PR title
     #[clap(short = 't', long)]
     pub pr_title: String,
 
-    /// The pr link
+    /// The PR link
     #[clap(short = 'l', long)]
     pub pr_link: String,
 }
